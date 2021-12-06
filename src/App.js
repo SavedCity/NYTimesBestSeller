4892b4188c948a9ee435af0aece9e47641ff4975
Andy Checo <savedcity@Andys-MBP.lan>
2021-12-06 09:57:33 -0500

    different routes and categories file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Books from "./components/Books";
+import Categories from "./components/Categories";
 
 function App() {
   const [booksData, setBooksData] = useState([]);
@@ -27,17 +28,15 @@ function App() {
       });
   };
 
-  if (!loading) {
-    console.log(booksData);
-  }
-
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" />
+          <Route path="/category/:categoryId" />
         </Routes>
         {!loading ? <Books booksData={booksData} /> : <h1>LOADING...</h1>}
+        <Categories loading={loading} booksData={booksData} />
       </Router>
     </div>
   );

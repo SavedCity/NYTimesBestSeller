0a601f5e37b30f11a929e5c8d95f2ebb4486f4ef
Andy Checo <savedcity@Andys-MBP.lan>
2021-12-27 13:26:33 -0500

    caurosel css and dots

--- src/components/headerComponents/HomeCategories.js
+++ src/components/headerComponents/HomeCategories.js
@@ -6,6 +6,12 @@ import Carousel from "react-grid-carousel";
 
 const CategoryBox = styled.div`
   margin: 10px;
+  border: 1px solid #0003;
+  padding: 20px;
+`;
+
+const CarouselContainer = styled.div`
+  margin: 50px 0;
 `;
 
 export default function HomeCategories() {
@@ -30,31 +36,69 @@ export default function HomeCategories() {
   };
   console.log(homeData);
 
+  const MyDot = ({ isActive }) => (
+    <span
+      style={{
+        height: isActive ? "8px" : "5px",
+        width: isActive ? "8px" : "5px",
+        background: "#6e44ff",
+        borderRadius: "50%",
+      }}
+    ></span>
+  );
+
+  let allSections = { us: "us", tech: "technology" };
+
+  const usFilter = homeData
+    .filter((about) => about.section === allSections.us)
+    .map((topStories) => {
+      const { title, section, abstract, byline, url } = topStories;
+      return (
+        <Carousel.Item>
+          <CategoryBox>
+            <h5>{byline}</h5>
+            <h3>{section}</h3>
+            <h2>{title}</h2>
+            <p>{abstract}</p>
+            <a href={url} target="_blank" rel="noopener noreferrer">
+              LINK
+            </a>
+          </CategoryBox>
+        </Carousel.Item>
+      );
+    });
+
+  const techFilter = homeData
+    .filter((about) => about.section === allSections.tech)
+    .map((topStories) => {
+      const { title, section, abstract, byline, url } = topStories;
+      return (
+        <Carousel.Item>
+          <CategoryBox>
+            <h5>{byline}</h5>
+            <h3>{section}</h3>
+            <h2>{title}</h2>
+            <p>{abstract}</p>
+            <a href={url} target="_blank" rel="noopener noreferrer">
+              LINK
+            </a>
+          </CategoryBox>
+        </Carousel.Item>
+      );
+    });
+
   return (
     <div>
       {!loading ? (
-        <div style={{ width: "500px" }}>
-          <Carousel cols={1} rows={1} gap={1} loop>
-            {homeData
-              .filter((about) => about.section === "us")
-              .map((topStories) => {
-                const { title, section, abstract, byline, url } = topStories;
-                return (
-                  <Carousel.Item>
-                    <CategoryBox>
-                      <h5>{byline}</h5>
-                      <h3>{section}</h3>
-                      <h2>{title}</h2>
-                      <p>{abstract}</p>
-                      <a href={url} target="_blank" rel="noopener noreferrer">
-                        LINK
-                      </a>
-                      <hr />
-                    </CategoryBox>
-                  </Carousel.Item>
-                );
-              })}
+        <div style={{ width: "90%", margin: "0 auto" }}>
+          <Carousel cols={2} rows={1} gap={10} loop showDots dot={MyDot}>
+            {usFilter}
           </Carousel>
+          <CarouselContainer>
+            <Carousel cols={2} rows={1} gap={20} loop showDots dot={MyDot}>
+              {techFilter}
+            </Carousel>
+          </CarouselContainer>
         </div>
       ) : (
         <div className="loader-div">
